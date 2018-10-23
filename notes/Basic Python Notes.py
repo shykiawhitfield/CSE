691dca61ff5155ad17b92cce0e2774712b0e8d6d
Shykia Whitfield 6308 <cose@fresnou.org>
2018-10-23 15:18:58 -0700

    Missing Notes From Class

---
+++ notes/Basic Python Notes.py
@@ -0,0 +1,35 @@
+print("Hello World!")
+print()
+# This is a comment. I can write whatever I want
+# Here and it won't do anything about it.
+# It has no effect on the code.
+print()  # This adds extra spaces on the terminal
+print("This will print here. Notice the spacing.")
+
+a = 4
+b = 3
+print(a + b)
+print(a * 5)
+print(5 - 3)
+print(6 / 5)  # Results in a float (with decimals)
+
+
+print("Figure this out")
+print(6 // 4)  # Results in a integer (Without decimals)
+print(12 // 3)
+print(9 // 4)
+
+
+# MORE MATH STUFF
+
+# Variables
+
+car_name = "The Wiebe Mobile"
+car_type = "Tesla"
+car_cylinders = 1024
+car_miles_per_gallon = 0.01
+print("I have a car called %s. It's pretty nice." % car_name)
+
+# Input
+name = input("What is your name? ")
+print("Hello %s" % name)
